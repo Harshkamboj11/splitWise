fa15c221cbeee287a85064ecdc28c436b74b68ce
harshkamboj11 <hk8994854@gmail.com>
2025-12-31 13:28:09 +0530

    FIX: fixed the route to the signup

--- server/auth-service/src/interface/http/auth.route.js
+++ server/auth-service/src/interface/http/auth.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import {handleLogin, handleSignUp} from './auth.controller.js';
 
-router.post('/user/create-new-user', handleSignUp);
+router.post('/user/signup', handleSignUp);
 router.post('/user/login', handleLogin);
 
 export default router;
