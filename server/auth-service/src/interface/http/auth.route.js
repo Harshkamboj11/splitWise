d88eab9a0283eddf96499b20ffd7ef3a4d4c50e4
harshkamboj11 <hk8994854@gmail.com>
2026-01-01 14:29:00 +0530

    BUILD: Google Login Routes

--- server/auth-service/src/interface/http/auth.route.js
+++ server/auth-service/src/interface/http/auth.route.js
@@ -1,9 +1,46 @@
 import express from 'express';
+import passport from 'passport';
+import generateToken from '../../infrastructure/security/token.provider.js';
+
 const router = express.Router();
 
-import {handleLogin, handleSignUp} from './auth.controller.js';
+import { handleLogin, handleSignUp } from './auth.controller.js';
 
 router.post('/user/signup', handleSignUp);
 router.post('/user/login', handleLogin);
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    session: false,
+  })
+);
+
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    session: false,
+    failureRedirect: '/',
+  }),
+  (req, res) => {
+    const user = req.user;
+
+    const token = generateToken({
+      userId: user.id,
+      email: user.email,
+    });
+
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: 'strict',
+      secure: false, // true in prod
+    });
+
+    res.redirect('');
+  }
+);
 
 export default router;
