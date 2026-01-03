33988d7ec676b37d243df592f7bcd69ff1c1f891
harshkamboj11 <hk8994854@gmail.com>
2026-01-03 20:22:31 +0530

    BUILD: auth middleware

---
+++ server/auth-service/src/interface/middleware/auth.middleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+const verifyUser = async (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  try {
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: 'unauthorized',
+      });
+    }
+
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: 'Invalid or expired token',
+      error: error.message,
+    });
+  }
+};
+
+export default verifyUser;
