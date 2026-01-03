45f6cc849bd0aa7cb8cc0be7fccdaf1c551aab1a
harshkamboj11 <hk8994854@gmail.com>
2026-01-03 20:23:20 +0530

    BUILD: Made Password Hasher

--- server/auth-service/src/infrastructure/security/password.hasher.js
+++ server/auth-service/src/infrastructure/security/password.hasher.js
@@ -1,11 +1,20 @@
 import bcrypt from 'bcryptjs';
+import 'dotenv/config';
 
 //It will encrypt the password
 const encryptPass = async (password) => {
-  const salt_round = 12;
+  const salt_round = Number(process.env.SALT_ROUNDS) || 10;
   const hashPass = await bcrypt.hash(password, salt_round);
 
   return hashPass;
 };
 
-module.exports = encryptPass;
+const decryptPass = async (pass, hashPass) => {
+  const compare = await bcrypt.compare(pass, hashPass);
+
+  if (compare) return true;
+
+  return false;
+};
+
+export { encryptPass, decryptPass };
