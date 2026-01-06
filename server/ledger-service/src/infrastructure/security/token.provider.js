70c69d78d7fdabc6cecdc3ddaf7df81c3e761e65
harshkamboj11 <hk8994854@gmail.com>
2026-01-06 19:46:55 +0530

    FIX: Made Some Changes In Algorithm

---
+++ server/ledger-service/src/infrastructure/security/token.provider.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+const generateToken = async (payload) => {
+  try {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: '168h' //7 days
+    });
+    console.log(token);
+    return token;
+  } catch (error) {
+    console.log('error in generating token', error);
+  }
+};
+
+export default generateToken;
