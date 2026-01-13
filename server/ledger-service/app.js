c1f80900ff7b92f01b2b4648e02b9d4ee4f5321b
harshkamboj11 <hk8994854@gmail.com>
2026-01-13 21:52:25 +0530

    BUILD: made app

---
+++ server/ledger-service/app.js
@@ -0,0 +1,27 @@
+//modules imports
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+//other imports
+import addUsers from './src/interface/http/ledger.route.js';
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
+app.use('/api/user', addUsers);
+app.get('/', (req, res) => {
+  res.json('helllo');
+  console.log('hii u are at homepage');
+});
+
+export default app;
