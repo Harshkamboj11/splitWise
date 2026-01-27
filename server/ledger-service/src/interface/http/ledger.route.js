2157881907eda15682fa5af37c4b7fb3e30ac05c
harshkamboj11 <hk8994854@gmail.com>
2026-01-27 08:33:12 +0530

    BUILD: get participants route

--- server/ledger-service/src/interface/http/ledger.route.js
+++ server/ledger-service/src/interface/http/ledger.route.js
@@ -1,11 +1,17 @@
 import express from 'express';
 const router = express.Router();
 
-import { addAmount, splitAmount, addParticipant} from './ledger.controller.js';
-import verifyUser from '../middleware/auth.middleware.js'
+import {
+  addAmount,
+  splitAmount,
+  addParticipant,
+  getParticipants,
+} from './ledger.controller.js';
+import verifyUser from '../middleware/auth.middleware.js';
 
-router.post('/add-users',verifyUser, addParticipant);
-router.post('/add-amount',verifyUser, addAmount);
-router.get('/split-amount/:participantName',verifyUser, splitAmount);
+router.post('/add-users', verifyUser, addParticipant);
+router.post('/add-amount', verifyUser, addAmount);
+router.get('/split-amount/:participantName', verifyUser, splitAmount);
+router.get('/participants', verifyUser, getParticipants);
 
 export default router;
